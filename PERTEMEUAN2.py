17b17c27000254b1100bf571311a52615a3db7b7
Faudzan10 <irdhan.faudzan@gmail.com>
2024-09-24 14:11:57 +0800

    Add files via upload

---
+++ PERTEMEUAN2.py
@@ -0,0 +1,11 @@
+for alfabet in "abcdefghijklmnopqrstuvwxyz":
+    print(alfabet)
+
+for bola in range (1):
+    print(bola)
+
+bola = ("basket","tennis","pingpong")
+print(bola)
+for bola in bola:
+    if bola == "basket":
+        print(bola)
